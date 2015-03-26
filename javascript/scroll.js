720ea29f65ced0b5323d09c2aa3dc69a0bcf95c7
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-03-26 18:03:05 -0400

    Added intro quote, rejigged the scroll js script

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,9 +1,13 @@
-var scrollLimit = 600;
+var scrollLimit = 500;
 
 $(window).on('scroll', function(){
 	var scrollPos = $(this).scrollTop();
 
 	if(scrollPos < scrollLimit) {
-		$('.title').css({'opacity' : (1 - scrollPos / scrollLimit)})
+		$('.landing-bg').css({'opacity' : (1 - scrollPos / scrollLimit)})
+	}
+
+	if(scrollPos > scrollLimit) {
+		$('.landing-bg').css({'opacity' : 0})
 	}
 });
