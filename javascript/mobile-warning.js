772571f56b12efb88a5dd4eeb57c7b4004e76c2a
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-08 17:30:47 -0400

    Added mobile landscape notification

---
+++ javascript/mobile-warning.js
@@ -0,0 +1,11 @@
+if ( ($(window).width() < 768) && ($(window).height() > $(window).width())) {
+	setTimeout(function(){
+		$('.mobile-container').addClass("rotate");
+	}, 500);
+
+	setTimeout(function(){
+		$('.mobile').fadeOut("fast");
+	}, 5000);
+} else {
+	$('.mobile').css("display", "none");
+}
